c7623b920e1e25de1ddb9d1f349482620197820e
Oluwpelumi <tijaniabdulbasit111@gmail.com>
2022-08-19 09:19:22 +0100

    url update

--- smproject/urls.py
+++ smproject/urls.py
@@ -25,4 +25,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('smapp.urls')),
 
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
