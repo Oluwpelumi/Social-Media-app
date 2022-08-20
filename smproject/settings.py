5985c66f71c0ed4cf9b01af57342f8681b5234e6
Oluwpelumi <tijaniabdulbasit111@gmail.com>
2022-08-20 10:31:55 +0100

    smd project

--- smproject/settings.py
+++ smproject/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +28,7 @@ SECRET_KEY = 'django-insecure-@kx+-bpzx45edz7dn13lngz6ooz0ka7dny&cn3g%agqq7zza_)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1' ,'https://adewale-smd.herokuapp.com/']
+ALLOWED_HOSTS = ['127.0.0.1' ,'https://adewale-smd.com/']
 
 
 # Application definition
@@ -142,3 +144,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 AUTH_PROFILE_MODULE = 'smapp.Profile'
 
+
+django_heroku.settings(locals())
